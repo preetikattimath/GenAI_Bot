3c84ebd926b9afa613bdd7a16e2da8fa91110206
preetikattimath <preetikattimath@gmail.com>
2026-04-15 10:43:19 +0530

    Initialize App.py with Streamlit setup

---
+++ App.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from greg import Greg
+
+st.set_page_config("Preeti AI Content Generator", layout="wide")
+st.title("Preeti AI - Content Generator")
+st.image("download (1).jpg")
